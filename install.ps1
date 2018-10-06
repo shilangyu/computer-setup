08ac19d7528c3ef064cb970b3f3cfee86e687e1c
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-06 17:25:32 +0200

    installing programs first

--- install.ps1
+++ install.ps1
@@ -1,5 +1,9 @@
+Set-Location .\programs
+& .\install.ps1
+Set-Location ..
+
 Get-ChildItem | Select-Object Name | ForEach-Object {
-	If($_.Name -match "install.ps1" -or $_.Name -match "README.md") {
+	If($_.Name -match "install.ps1" -or $_.Name -match "README.md" -or $_.Name -match "programs") {
 	} else {
 		Set-Location .\$($_.Name)
 		& .\install.ps1
