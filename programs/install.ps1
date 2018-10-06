8756c1839c9e15895fbee4a8676f8aae1af81e49
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-06 14:40:06 +0200

    added python

--- programs/install.ps1
+++ programs/install.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$programs = 'git', 'vscode', 'golang', 'googlechrome', 'nodejs.install'
+$programs = 'git', 'vscode', 'golang', 'googlechrome', 'nodejs.install', 'python'
 
 foreach($program in $programs) {
 	choco install $program
