68f0145e47bdebee5b1b64ec9955cc82a50e82f0
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-06 17:00:05 +0200

    fixed to string error

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 Get-ChildItem | Select-Object Name | ForEach-Object {
 	If($_.Name -match "install.ps1" -or $_.Name -match "README.md") {
 	} else {
-		Set-Location .\$_.Name
+		Set-Location .\$($_.Name)
 		& .\install.ps1
 	}
 	Set-Location ..
