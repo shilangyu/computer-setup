4a71b416a13196e3cf87f876987582abb3e8f3b2
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-08 23:35:52 +0200

    removed unused filter

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ Set-Location .\programs
 & .\install.ps1
 Set-Location ..
 
-Get-ChildItem | Select-Object Name | ForEach-Object {
+Get-ChildItem | ForEach-Object {
 	If($_.Name -match "install.ps1" -or $_.Name -match "README.md" -or $_.Name -match "programs") {
 	} else {
 		Set-Location .\$($_.Name)
