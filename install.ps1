10414f69a23d26e5882636d8223caabe1389e7da
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-14 13:20:01 +0200

    more explicit

--- install.ps1
+++ install.ps1
@@ -2,9 +2,8 @@ Set-Location .\programs
 & .\install.ps1
 Set-Location ..
 
-Get-ChildItem | ForEach-Object {
-	If($_.Name -match "install.ps1" -or $_.Name -match "README.md" -or $_.Name -match "programs") {
-	} else {
+Get-ChildItem -Directory | ForEach-Object {
+	If($_.Name -ne "programs") {
 		Set-Location .\$($_.Name)
 		& .\install.ps1
 		Set-Location ..
