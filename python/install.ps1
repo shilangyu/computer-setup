891237df185a02b8747fbd60e392a014299fce4d
Marcin Wojnarowski <xmarcinmarcin@gmail.com>
2018-10-14 13:44:05 +0200

    python global packages

--- python/install.ps1
+++ python/install.ps1
@@ -1,3 +1,10 @@
+$config = Get-Content config.json | ConvertFrom-Json
+
+# global packages
+foreach($pckg in $config.global) {
+	python -m pip install $pckg
+}
+
 # wheel files
 Get-ChildItem .\data | ForEach-Object {
 	python -m pip install ./data/$_
