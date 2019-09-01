32300db16b137163008fb8cab44f767aaf0ad6ee
shilangyu <xmarcinmarcin@gmail.com>
2019-09-01 21:14:24 +0200

    transitioning to scoop

--- programs/install.ps1
+++ programs/install.ps1
@@ -1,9 +1,9 @@
-# install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# install scoop
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 $config = Get-Content config.json | ConvertFrom-Json
 
-install programs
+# install programs
 foreach ($program in $config.programs) {
 	if ($program.params.length -eq 0) {
 		choco.exe install $program.name -y
