9a3e22429f247dabfa63fa48279d79137cc444e4
shilangyu <xmarcinmarcin@gmail.com>
2019-09-02 11:21:27 +0200

    changed choco to scoop

--- programs/install.ps1
+++ programs/install.ps1
@@ -1,14 +1,15 @@
 # install scoop
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
 
 $config = Get-Content config.json | ConvertFrom-Json
 
+# add buckets
+foreach ($bucket in $config.buckets) {
+	scoop bucket add $bucket
+}
+
 # install programs
 foreach ($program in $config.programs) {
-	if ($program.params.length -eq 0) {
-		choco.exe install $program.name -y
-	}
-	else {
-		choco.exe install $program.name -y --params "$([string]::Join(" ", $program.params))"
-	}
+	scoop install $program
 }
