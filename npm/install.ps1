a9f4bb05f2b8455d60ed52fb11bb57360f27c34d
shilangyu <xmarcinmarcin@gmail.com>
2019-09-02 11:25:54 +0200

    installing pnpm first

--- npm/install.ps1
+++ npm/install.ps1
@@ -1,6 +1,9 @@
 $config = Get-Content config.json | ConvertFrom-Json
 
+# install pnpm first
+npm i -g pnpm
+
 # global packages
-foreach($pckg in $config.global) {
-	npm i -g $pckg 
+foreach ($pckg in $config.global) {
+	pnpm i -g $pckg 
 }
