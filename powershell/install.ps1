13b4343000d1dc858c86e6bcda95ea1ea6aeaf11
shilangyu <xmarcinmarcin@gmail.com>
2019-09-05 11:45:38 +0200

    startup script works

--- powershell/install.ps1
+++ powershell/install.ps1
@@ -4,6 +4,5 @@ $config = Get-Content config.json | ConvertFrom-Json
 Copy-Item ./data/profile.ps1 $profile
 
 # create startup script
-$startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-Copy-Item ./data/startup.ps1 "$startupPath/startup.ps1"
-[IO.File]::WriteAllLines("$startupPath/startup.bat", "@ECHO OFF`nPowerShell.exe -Command `"& '$startupPath/startup.ps1'`"")
+Copy-Item ./data/startup.ps1 ~
+[IO.File]::WriteAllLines("$env:APPDATA/Microsoft/Windows/Start Menu/Programs/Startup/startup.bat", "@ECHO OFF`nPowerShell.exe -Command `"~/startup.ps1`"")
